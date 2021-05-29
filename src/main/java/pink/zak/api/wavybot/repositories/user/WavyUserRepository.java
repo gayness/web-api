5f979293457fe2b8d90dcb466a089d0fab76930c
Zak <shearmanzak@gmail.com>
2021-05-29 02:35:04 +0100

    Add find WavyUser by User query

--- src/main/java/pink/zak/api/wavybot/repositories/user/WavyUserRepository.java
+++ src/main/java/pink/zak/api/wavybot/repositories/user/WavyUserRepository.java
@@ -2,10 +2,13 @@ package pink.zak.api.wavybot.repositories.user;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
+import pink.zak.api.wavybot.models.user.User;
 import pink.zak.api.wavybot.models.user.WavyUser;
 
 import java.util.UUID;
 
 @Repository
 public interface WavyUserRepository extends MongoRepository<WavyUser, UUID> {
+
+    WavyUser findByUserIs(User user);
 }
